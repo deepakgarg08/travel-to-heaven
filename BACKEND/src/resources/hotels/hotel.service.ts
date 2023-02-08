f2d0c981588caf843a235d3bb232f03553478464
Mystic Research <mysticresearcher@gmail.com>
2023-02-08 16:33:20 +0100

    Add HaversineDistanece and language based fallbacks

--- BACKEND/src/resources/hotels/hotel.service.ts
+++ BACKEND/src/resources/hotels/hotel.service.ts
@@ -1,15 +1,49 @@
-import { IParameters, Result } from './hotel.interface';
+import {IParameters, Result} from './hotel.interface';
 import dummyData from '../../data/hotels.json';
-import { ResponseObject } from './hotel.interface';
+import {ResponseObject} from './hotel.interface';
+import haversineDistance from "haversine-distance";
+
 
 class HotelService {
     // stores final response result
     private finalResponse: ResponseObject[] = [];
 
+    private calculateHaversineDistance(
+        latitude: number,
+        longitude: number
+    ): number {
+        const berlinCordinates: { latitude: number; longitude: number } = {
+            latitude: 37.8136,
+            longitude: 144.9631,
+        };
+        const hotelLocation: { latitude: number; longitude: number } = {
+            latitude,
+            longitude,
+        };
+        return haversineDistance(berlinCordinates, hotelLocation);
+    }
+
+    private getResponseBasedOnlanguage(obj: any, lang: string): string {
+        // console.log('lang: ', lang);
+        // console.log('obj: ', obj); 
+        const languages = ["en-US", "de-DE", "fr-FR", "es-ES"];
+        let res: string = obj[lang as keyof typeof obj] as string
+        if (res) {
+            return res
+        }
+        for (const language of languages) {
+            const result = obj[language as keyof typeof obj] as string
+            if (result !== null) {
+                return result
+            }
+        }
+        return res
+    }
+
     private async filterHotels(
         requestParams: IParameters
     ): Promise<ResponseObject[]> {
-        let { hotelId, lang = 'en-US', search } = requestParams;
+        let {hotelId, lang = 'en-US', search} = requestParams;
         let result;
         this.finalResponse = [];
         let responseObject!: ResponseObject;
@@ -22,18 +56,17 @@ class HotelService {
             result = await dummyData.find((hotel) => hotel.id === hotelId);
 
             if (result) {
-                const { id, name, address, city, description, minPrice, currencyCode, deals, images, lat, lng } = result;
+                const {id, name, address, city, description, minPrice, currencyCode, deals, images, lat, lng} = result;
+                const distanceToCenterKm: number = this.calculateHaversineDistance(lat, lng)
+
                 responseObject = {
                     id,
-                    name: name[lang as keyof typeof name] as string,
-                    address: address[
-                        lang as keyof typeof address
-                    ] as string,
-                    city: city[lang as keyof typeof city] as string,
-                    description: description[
-                        lang as keyof typeof description
-                    ] as string,
+                    name: this.getResponseBasedOnlanguage(name, lang),
+                    address: this.getResponseBasedOnlanguage(address, lang),
+                    city: this.getResponseBasedOnlanguage(city, lang),
+                    description: this.getResponseBasedOnlanguage(description, lang),
                     minPrice,
+                    distanceToCenterKm,
                     currencyCode,
                     deals: [{
                         headline: deals.map(deal => deal.headline[lang as keyof typeof deal.headline])[0] || "",
@@ -85,10 +118,10 @@ class HotelService {
     public async getHotels(requestParams: IParameters): Promise<Result> {
         try {
             const result = await this.filterHotels(requestParams);
-            
-            return { success: true, error: '', result };
+
+            return {success: true, error: '', result};
         } catch (error: any) {
-            
+
             throw new Error(error.message);
         }
     }
