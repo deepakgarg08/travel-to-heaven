8a376fd4c09a39937e7df2f95376365456577fa5
Mystic Research <mysticresearcher@gmail.com>
2023-02-09 16:26:40 +0100

    reactjs initialisation

---
+++ FRONTEND/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     Lets Plan for the next travelling plan to Heaven
+    </div>
+  );
+}
+
+export default App;
